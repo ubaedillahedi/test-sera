bbb9256f39433c3f2b7fc73577ad0092a9436b6e
Edi Ubaedillah <ubaedillahedi@gmail.com>
2022-04-21 00:02:54 +0700

    filter object

---
+++ app/Http/Controllers/FilterObjectController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FilterObjectController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $response = [];
+        $json = json_decode(file_get_contents(__DIR__ . '/../../../' . env('FILTER_DATA')));
+        foreach ($json->data->response->billdetails as $val) {
+            $number = preg_replace('/DENOM|:|\s+/', '', $val->body[0]);
+            if ((int) $number >= 100000) $response[] = (int) $number;
+        }
+        var_dump($response);
+        return true;
+    }
+}
